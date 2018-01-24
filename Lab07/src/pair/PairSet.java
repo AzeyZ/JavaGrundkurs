8f27b4e9e500aab7fc3dd4b0134a14cb27eb4ff7
Andreas <Andreas@Andreas-PC>
2018-01-24 21:20:58 +0100

    adding original Lab07

---
+++ Lab07/src/pair/PairSet.java
@@ -0,0 +1,20 @@
+package pair;
+
+public class PairSet {
+
+	/** Skapar en mängd av alla talpar (a,b) sådana att
+        0 <= a < rows  och  0 <= b < cols */
+	public PairSet(int rows, int cols) {
+	}
+
+	/** Undersöker om det finns fler par i mängden. */
+	public boolean more() {
+		return false;
+	}
+
+	/** Hämtar ett slumpmässigt valt talpar ur mängden. Mängden
+	 	blir ett element mindre. Om mängden är tom returneras null. */
+	public Pair pick() {
+		return null;
+	}
+}
