7c931b62e72f8ea10613627d600b8f71edf1c686
Andreas <Andreas@Andreas-PC>
2018-01-24 21:27:13 +0100

    Adding the rest of the original WorkSpace

---
+++ Lab01/src/LineDrawing.java
@@ -0,0 +1,12 @@
+import se.lth.cs.pt.window.SimpleWindow;
+
+public class LineDrawing {
+	public static void main(String[] args) {
+		SimpleWindow w = new SimpleWindow(500, 500, "LineDrawing");
+		w.moveTo(0, 0);
+		while (true) {
+			// vänta tills användaren klickar på en musknapp
+			// rita en linje till den punkt där användaren klickade
+		}
+	}
+}
