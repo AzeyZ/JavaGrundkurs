d50f6cb2669d3a6d1b7db5f7369082039c7efcc2
Andreas <Andreas@Andreas-PC>
2018-01-25 23:03:05 +0100

    Adding constructor and createCards()

--- Lab08/src/memory/MemoryBoard.java
+++ Lab08/src/memory/MemoryBoard.java
@@ -1,41 +1,70 @@
 package memory;
 
+import java.util.Random;
 
 public class MemoryBoard {
-	private int[][] board;
+	private MemoryCardImage[][] board;
+	private int size;
+	private String backFileName;
+	private String[] frontFileNames;
 	
 
 	/** Skapar ett memorybräde med size * size kort. backFileName är filnamnet 
 	    för filen med baksidesbilden. Vektorn frontFileNames innehåller filnamnen 
 	    för frontbilderna. */
 	public MemoryBoard(int size, String backFileName, String[] frontFileNames) {
-		this.board = new int[size][size];
-		for(int i=0;i<size*size;i++) {
-			MemoryCardImage cardImage = new MemoryCardImage(backFileName,frontFileNames[i]);
-		}
+		this.board = new MemoryCardImage[size][size];
+		this.size = size;
+		this.backFileName = backFileName;
+		this.frontFileNames = frontFileNames;
+
 		
 	}
 
 	/* Skapar size * size / 2 st memorykortbilder.
 	   Placerar ut varje kort på två slumpmässiga ställen på spelplanen. */
 	private void createCards(String backFileName, String[] frontFileNames) {
-
+		
+			String[] framsidor = new String[size*size];
+			int factor = 0;
+			for (int i = 0; i < framsidor.length; i++) {
+				framsidor[i] = frontFileNames[factor];
+				if(i == ((size*size/2)-1)) {
+					factor=0;
+				}
+				factor++;
+			}
+			
+			Random r = new Random();
+			int n = size*size;
+			for(int rows = 0; rows < size; rows++) {
+				
+				for (int cols = 0; cols < size; cols++) {
+					int rand = r.nextInt(n);
+					this.board[rows][cols] = new MemoryCardImage(framsidor[rand],backFileName);
+					frontFileNames[rand] = frontFileNames[rand-1];
+					frontFileNames[rand-1] =null;				
+					n--;
+				}
+			}
+			
 	}
 
 	/** Tar reda på brädets storlek. */
 	public int getSize() {
-		return 0;
+		return size;
 	}
 	
 	/** Hämtar den tvåsidiga bilden av kortet på rad r, kolonn c.
 	    Raderna och kolonnerna numreras från 0 och uppåt. */
 	public MemoryCardImage getCard(int r, int c) {
-		return null;
+		
+		return board[r][c];
 	}
 
 	/** Vänder kortet på rad r, kolonn c. */
 	public void turnCard(int r, int c) {
-
+		
 	}
 	
 	/** Returnerar true om kortet r, c har framsidan upp. */
